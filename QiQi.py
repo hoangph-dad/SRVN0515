c9029bceda277439f2c94576c637448c177e34bb
hoangph-dad <hoangph.385@gmail.com>
2024-12-17 03:09:05 +0700

    Update QiQi.py

--- QiQi.py
+++ QiQi.py
@@ -1,19 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Render!"
-
 @app.route('/callback', methods=['POST'])
 def callback():
-    data = request.json
+    data = request.get_json()
+    if not data:
+        return "No JSON data received", 400
+
     seatalk_challenge = data.get('seatalk_challenge')
     if seatalk_challenge:
         return seatalk_challenge, 200
-    return "Invalid request", 400
+    return "seatalk_challenge not found in request", 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
