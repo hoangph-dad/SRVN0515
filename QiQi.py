4c1c3444be2e30c41fd35ff62072ed1505d8dd87
hoangph-dad <hoangph.385@gmail.com>
2024-12-17 03:12:25 +0700

    Update QiQi.py

--- QiQi.py
+++ QiQi.py
@@ -6,12 +6,15 @@ app = Flask(__name__)
 @app.route('/callback', methods=['POST'])
 def callback():
     data = request.get_json()
+    print("Received data:", data)  # In dữ liệu nhận được ra console để debug
+
     if not data:
         return "No JSON data received", 400
 
     seatalk_challenge = data.get('seatalk_challenge')
     if seatalk_challenge:
         return seatalk_challenge, 200
+
     return "seatalk_challenge not found in request", 400
 
 if __name__ == '__main__':
